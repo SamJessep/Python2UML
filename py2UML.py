1eb5505002a068ee6c21d92559c48fbdb0617194
SamJessep <samjessep77@gmail.com>
2020-03-15 15:55:39 +1300

    fixed make_diagram method
    method now uses python module, reads from file and folders, still need to add error handling for files

--- py2UML.py
+++ py2UML.py
@@ -7,9 +7,10 @@
 # src = src.from_file('classes.dot');
 # src.render(format='png', filename=diagram_file_name);
 from argparse import ArgumentParser
-from glob import glob;
+from glob import glob
 from os import system
 from shutil import move
+from graphviz import Source
 
 from autopep8 import fix_code
 
@@ -21,8 +22,8 @@ parser.add_argument("-n", "--DiagramName", help="name for diagram when its saved
 outputHelp = 'set output file type e.g "-e png"' \
              "supported file types: png, pdf, ps, svg, svgz, fig, mif, hpgl, pcl, gif, dia, imap, cmapx"
 parser.add_argument("-e", "--Extention", help=outputHelp)
-parser.add_argument("-c", "--ClassOnly", help="creates diagram for classes only and ignores packages");
-parser.add_argument("-p", "--PackageOnly", help="creates diagram for packages only and ignores classes");
+parser.add_argument("-c", "--ClassOnly", help="creates diagram for classes only and ignores packages")
+parser.add_argument("-p", "--PackageOnly", help="creates diagram for packages only and ignores classes")
 args = parser.parse_args()
 
 
@@ -46,27 +47,33 @@ class Py2UML:
         self.out_file_type = out_file_type
         self.name = diagram_name
 
-    def read_source_code(self):
-        return glob(f'{self.in_path}/**/*.py');
+    def get_python_files(self):
+        if ".py" in self.in_path:
+            return [self.in_path]
+        return glob(f'{self.in_path}/**.py')
 
     def add_files_to_buffer_file(self, files):
         temp_buffer = ''
         for a_file in files:
-            temp_buffer += IO.read(a_file);
-        IO.write('buffer.py', self.clean_code(temp_buffer));
+            temp_buffer += IO.read(a_file)
+        IO.write('buffer.py', self.clean_code(temp_buffer))
         return temp_buffer
+
+    def create_buffer(self):
+        python_files = self.get_python_files()
+        print(python_files)
+        return self.add_files_to_buffer_file(python_files)
 
     @staticmethod
     def clean_code(code):
-        return fix_code(code);
+        return fix_code(code)
 
-    def make_diagram(self):
-        # make diagram in default location then move to desired location
-        print(self.in_path, self.out_path);
-        system(f'pyreverse {self.in_path} -o {self.out_file_type} -p {self.name}');
-        move(f'./classes_{self.name}.{self.out_file_type}', f'{self.out_path}/classes_{self.name}.{self.out_file_type}')
-        move(f'./packages_{self.name}.{self.out_file_type}',
-             f'{self.out_path}/packages_{self.name}.{self.out_file_type}')
+    def make_dot(self, buffer_path):
+        system(f'pyreverse {buffer_path}  -p {self.name}')
+
+    def make_diagram(self, dot_path):
+        src = Source(IO.read(dot_path))
+        src.render(format=self.out_file_type, filename=self.name, directory=self.out_path)
 
 
 print(__name__)
@@ -74,10 +81,12 @@ if __name__ == "__main__":
     optional_args = {}
 
     if args.DiagramName:
-        optional_args += {"diagram_name": args.DiagramName}
+        optional_args = {"diagram_name": args.DiagramName}
 
     if args.Extention:
-        optional_args += {"out_file_type": args.Extention}
+        optional_args = {"out_file_type": args.Extention, **optional_args}
 
     p2u = Py2UML(in_path=args.SourceCodePath, out_path=args.OutputPath, **optional_args);
-    p2u.make_diagram();
+    p2u.create_buffer()
+    p2u.make_dot('buffer.py')
+    p2u.make_diagram(f'classes_{p2u.name}.dot')
