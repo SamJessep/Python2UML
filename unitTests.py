7a20ccd3767812a18c96ff984bb3d84d07d7e774
BlackMajor <henrydyer6@hotmail.com>
2020-03-23 13:40:32 +1300

    Edited componentOwnership doc, edited unit test file a smidgen. Quietly, embarassingly realising I need to ask for an extension.

--- unitTests.py
+++ unitTests.py
@@ -1,8 +1,14 @@
 import unittest
-from py2UML import Py2UML
 
+#Quick fire example unit test. Gotta add these for the main app later.
 class TestMethods(unittest.TestCase):
     def test_upper(self):
+        #Quick fire example doctest. Gotta integrate these into the app's commands.
+        """
+        >>> x = 1
+        >>> x
+        1
+        """
         print('Testing upper')
         self.assertEqual('foo'.upper(), 'FOO')
 
