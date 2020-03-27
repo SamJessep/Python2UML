97e2b75d3feadf9749bc8e97927f80399f4f3d97
SamJessep <samjessep77@gmail.com>
2020-03-27 21:21:50 +1300

    fixed some of the exceptions recommitted the __main__.py fix for moving the command line class to commandLine.py

--- IO.py
+++ IO.py
@@ -1,3 +1,7 @@
+from os import path
+from exceptions import InvalidPathError
+
+
 class IO:
 
     @staticmethod
@@ -9,3 +13,10 @@ class IO:
     def read(file_name, encode_type="utf8"):
         with open(file_name, 'r', encoding=encode_type) as file:
             return file.read()
+
+    @staticmethod
+    def try_path(file_path):
+        path_exists = path.exists(file_path)
+        if not path_exists:
+            raise InvalidPathError(path=file_path)
+        return path_exists
