80f8efc194aecb4e307b5264cf03be65a71e72ba
SamJessep <samjessep77@gmail.com>
2020-06-22 22:20:07 +1200

    implemented Builder pattern

--- decorations/show_after.py
+++ decorations/show_after.py
@@ -4,9 +4,16 @@ import decorator
 
 
 class ShowAfter(decorator.Decorator):
+    def __init__(self, BaseComponent, show_diagram, show_location):
+        super().__init__(BaseComponent)
+        self._show_diagram = show_diagram
+        self._show_location = show_location
 
-    def run(self):  # pragma: no cover
-        pass
+    def run(self):
+        if self._show_diagram:
+            self.show_diagram()
+        if self._show_location:
+            self.show_location()
 
     def show_diagram(self):
         cmd = f"{self.component.out_path}/{self.component.name}.{self.component.out_file_type}"
